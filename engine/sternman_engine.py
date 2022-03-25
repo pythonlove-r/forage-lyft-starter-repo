703100104e4126787248ee4dde0865dc2b8f64c5
pythonlove-r <74693532+pythonlove-r@users.noreply.github.com>
2022-03-24 21:30:25 -0400

    Update sternman_engine.py

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -3,12 +3,10 @@ from abc import ABC
 from car import Car
 
 
-class SternmanEngine(Car, ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
-        self.warning_light_is_on = warning_light_is_on
-
-    def engine_should_be_serviced(self):
+class SternEngineEngine(Engine,ABC):
+	def __init__(self, warning_light_on):
+		self.warning_light_is_on = warning_light_is_on
+    def needs_service(self):
         if self.warning_light_is_on:
             return True
         else:
