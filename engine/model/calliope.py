daa04b7f591ff2e9c8d6baa244689c1d8c489ff3
pythonlove-r <74693532+pythonlove-r@users.noreply.github.com>
2022-03-24 21:27:44 -0400

    Update calliope.py

--- engine/model/calliope.py
+++ engine/model/calliope.py
@@ -2,11 +2,38 @@ from datetime import datetime
 
 from engine.capulet_engine import CapuletEngine
 
-
-class Calliope(CapuletEngine):
-    def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+class CarFactory(object):
+	def __init__(self):
+		self.engine=Engine()
+		self.battery=Battery()
+		self.car=Car()
+	def create_calliope(current_date,last_service_date,current_mileage,last_service_mileague):
+        #order sensitive
+        capulet = CapuletEngine(current_mileage,last_service_mileague);
+        nubbin = NubbinBattery(last_service_date,current_date);     
+        calliope = Car(capulet,nubbin);
+        return calliope;
+        
+    def create_glissade(current_date,last_service_date,current_mileage,last_service_mileague):
+        willoughby = WilloughbyEngine(current_mileage, last_service_mileague);
+        nubbin = NubbinBattery(last_service_date,current_date);    
+        glissade= Car(willoughby,nubbin);
+        return glissade();
+        
+    def create_palindrome(current_date,last_service_date,warning_light_on):
+        stern = SternmanEngine(current_date,last_service_date,warning_light_on);
+        nubbin = NubbinBattery(last_service_date,current_date);    
+        palindrome = Car(stern,nubbin)
+        return palindrome;
+         
+    def create_rorschach(current_date,last_service_date,current_mileage,last_service_mileague):
+        willoughby = WilloughbyEngine(current_mileage, last_service_mileague);
+        nubbin = NubbinBattery(last_service_date,current_date); 
+        rorscheache = Car(willoughby,nubbin)
+        return rorscheache;
+         
+    def create_thovex(current_date,last_service_date,current_mileage,last_service_mileague):
+        capulet = CapuletEngine(current_mileage,last_service_mileague);
+        nubbin = NubbinBattery(last_service_date,current_date);     
+        thovex = Car(capulet,nubbin);
+        return  thovex;
